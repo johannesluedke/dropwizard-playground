7c9e4964d477b9b6612bada7950b787017aed11f
Johannes Lüdke <johannes.luedke@gmail.com>
2019-08-11 17:48:33 +0200

    start dropwizard project

---
+++ src/main/java/jol/dropwizard/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,47 @@
+package jol.dropwizard.helloworld;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Collections;
+import java.util.Map;
+
+public class HelloWorldConfiguration extends Configuration {
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @NotNull
+    private Map<String, Map<String, String>> viewRendererConfiguration = Collections.emptyMap();
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+
+
+}
